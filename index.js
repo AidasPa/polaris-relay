97e9e6decba6984488679de8a601a3b3f13fda4c
Aidas Patas <aidas0330@gmail.com>
2020-09-20 19:23:58 +0300

    event system

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ const io = require('socket.io')(http);
 const simConnect = require('../Projects/node-simconnect-master');
 
 const pullData = (data, fn, ...flags) => simConnect.requestDataOnSimObject(
-  Object.entries(data), (resp) => {
+  Object.entries(data).map(([key, value]) => {
+    if (typeof value === 'number') {
+      // special data type
+      return [key, null, value];
+    }
+    return [key, value];
+  }), (resp) => {
     fn(resp);
   }, ...flags,
 );
+
+const formatVars = (entries) => {
+  const spacedVars = {};
+  Object.entries(entries).forEach(([key, value]) => {
+    const spacedKey = key
+      .replace(/[A-Z]/g, (match) => ` ${match}`)
+      .toUpperCase();
+
+    if (/string|int|float/i.test(value)) {
+      // this is a non default data type
+      spacedVars[spacedKey] = simConnect.datatype[value.toUpperCase()];
+    } else {
+      spacedVars[spacedKey] = value;
+    }
+  });
+
+  return spacedVars;
+};
 
 function connectToSim() {
   const success = simConnect.open('MyAppName',
@@ -17,46 +41,65 @@ function connectToSim() {
       console.log(`Connected to: ${name}\nSimConnect version: ${version}`);
 
       io.on('connection', (socket) => {
-        // let frequency;
-        // let requestVars;
+        // console.log('connected');
+        socket.emit('connected', true);
+        socket.on('request', ({ vars, frequency = false, events }) => {
+          const spacedVars = formatVars(vars);
 
-        // socket.on('setUpdateFrequency', (interval) => {
-        //   frequency = interval;
-        // });
-        socket.on('request', ({ vars, frequency = false }) => {
-          const spacedVars = {};
-          console.log(spacedVars, frequency);
-          Object.entries(vars).forEach(([key, value]) => {
-            const spacedKey = key
-              .replace(/[A-Z]/g, (match) => ` ${match}`)
-              .toUpperCase();
-            spacedVars[spacedKey] = value;
-          });
-          if (frequency) {
-            let state = {};
+          const doDataPull = (fn) => {
             pullData(
               spacedVars,
-              (resp) => { state = resp; },
+              (resp) => { fn(resp); },
               simConnect.objectId.USER, // User aircraft
               simConnect.period.SIM_FRAME, // Get data every sim frame...
               simConnect.dataRequestFlag.CHANGED,
             );
+          };
+
+          events.forEach((e) => {
+            if (e === 'landed') {
+              let count = 0;
+              let oldValue;
+              doDataPull((resp) => {
+                if (count < 1) {
+                  oldValue = resp['SIM ON GROUND'];
+                }
+                count += 1;
+                if (resp['SIM ON GROUND'] === 1 && oldValue === 0) {
+                  socket.emit('landed', resp);
+                  oldValue = 1;
+                } else {
+                  oldValue = resp['SIM ON GROUND'];
+                }
+              });
+            } else if (e === 'tookOff') {
+              let count = 0;
+              let oldValue;
+              doDataPull((resp) => {
+                if (count < 1) {
+                  oldValue = resp['SIM ON GROUND'];
+                }
+                count += 1;
+                if (resp['SIM ON GROUND'] === 0 && oldValue === 1) {
+                  socket.emit('tookOff', resp);
+                  oldValue = 0;
+                } else {
+                  oldValue = resp['SIM ON GROUND'];
+                }
+              });
+            }
+          });
+
+          if (frequency) {
+            let state = {};
+            doDataPull((resp) => { state = resp; });
             setInterval(() => {
               socket.emit('update', state);
             }, frequency);
           } else {
-            pullData(
-              spacedVars,
-              (resp) => socket.emit('update', resp),
-              simConnect.objectId.USER, // User aircraft
-              simConnect.period.ONCE, // Get data every sim frame...
-              simConnect.dataRequestFlag.CHANGED,
-            );
+            doDataPull((resp) => { socket.emit('update', resp); });
           }
         });
-        // socket.on('start', (fn) => {
-
-        // });
       });
     }, () => {
       console.log('Simulator exited by user');
